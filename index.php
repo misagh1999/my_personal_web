51420474001094d6352b19d170981b4c6a2d6684
MohammadHossein MisaghPour <misagh1999@gmail.com>
2018-08-06 10:57:16 +0430

    add card template

--- index.php
+++ index.php
@@ -16,10 +16,10 @@
   }
 </style>
     <div class="container">
-      <div class="navbar navbar-expand">
+      <div class="navbar navbar-expand bg-dark navbar-dark mb-2 mt-1">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a href="#" class="nav-link">صفحه اصلی</a>
+            <a href="#" class="nav-link active">صفحه اصلی</a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">مقالات</a>
@@ -29,6 +29,65 @@
           </li>
         </ul><!--navbar nav-->
       </div><!--navbar-->
+      <div class="jumbotron text-justify">
+        <h1>به وب سایت من خوش آمدید</h1>
+        <p class="lead">تصمیم گرفتم که بعد از مدت ها اقدام به ایجاد یک وبسایت شخصی برای انعکاس نظرها و ایده هام کنم تا از این طریق بتونم اونا رو با شما هم به اشتراک بذارم. تو این سایت قصد دارم که از فعالیت ها, سرگرمی ها, علایق و تجربه ها و خاطراتم دلنوشته هایی رو با شما به اشتراک بذارم. همچنین سعی می کنم که مطالب آموزشی مفید شامل معرفی منابع, کتاب و مقالات علمی هم قرار بدم.تمام تلاشم بر اینه که توی این وبسایت مطالب دست اول و جدیدی رو قرار بدم و اونا رو از منظر خاصی بررسی کنم.
+از این که ما رو همراهی می کنید صمیمانه متشکرم :)
+</p>
+        <hr class="bg-danger">
+        <button type="button" class="btn btn-info float-left">شروع یادگیری</button>
+      </div><!-- jumbotron-->
+
+      <div class="row">
+        <div class="col-md-4 col-sm-6 mb-4">
+          <div class="card text-justify">
+            <div class="card-header">
+              <h4 class="card-title my-auto">معرفی سایت</h4>
+            </div>
+            <div class="card-body">
+              <p class="card-text">تصمیم گرفتم که بعد از مدت ها اقدام به ایجاد یک وبسایت شخصی برای انعکاس نظرها و ایده هام کنم تا از این طریق بتونم اونا رو با شما هم به اشتراک بذارم. تو این سایت قصد دارم که از فعالیت ها, سرگرمی ها, علایق و تجربه ها و خاطراتم دلنوشته هایی رو با شما به اشتراک بذارم.</p>
+              <a href="#" class="card-link btn btn-info float-left">ادامه مطلب...</a>
+            </div>
+          </div><!--card1-->
+        </div><!--col1-->
+        <div class="col-md-4 col-sm-6 mb-4">
+          <div class="card text-justify">
+            <div class="card-header">
+              <h4 class="card-title my-auto">اولین روز دانشگاه</h4>
+            </div>
+            <div class="card-body">
+              <p class="card-text">تصمیم گرفتم که بعد از مدت ها اقدام به ایجاد یک وبسایت شخصی برای انعکاس نظرها و ایده هام کنم تا از این طریق بتونم اونا رو با شما هم به اشتراک بذارم. تو این سایت قصد دارم که از فعالیت ها, سرگرمی ها, علایق و تجربه ها و خاطراتم دلنوشته هایی رو با شما به اشتراک بذارم.</p>
+              <a href="#" class="card-link btn btn-info float-left">ادامه مطلب...</a>
+            </div>
+          </div><!--card1-->
+        </div><!--col2-->
+        <div class="col-md-4 col-sm-6 mb-4">
+          <div class="card text-justify">
+            <div class="card-header">
+              <h4 class="card-title my-auto">اولین روز دانشگاه</h4>
+            </div>
+            <div class="card-body">
+              <p class="card-text">تصمیم گرفتم که بعد از مدت ها اقدام به ایجاد یک وبسایت شخصی برای انعکاس نظرها و ایده هام کنم تا از این طریق بتونم اونا رو با شما هم به اشتراک بذارم. تو این سایت قصد دارم که از فعالیت ها, سرگرمی ها, علایق و تجربه ها و خاطراتم دلنوشته هایی رو با شما به اشتراک بذارم.</p>
+              <a href="#" class="card-link btn btn-info float-left">ادامه مطلب...</a>
+            </div>
+          </div><!--card1-->
+        </div><!--col3-->
+        <div class="col-md-4 col-sm-6 mb-4">
+          <div class="card text-justify">
+            <div class="card-header">
+              <h4 class="card-title my-auto">بی انگیزگی در دانشگاه</h4>
+            </div>
+            <div class="card-body">
+              <p class="card-text">تصمیم گرفتم که بعد از مدت ها اقدام به ایجاد یک وبسایت شخصی برای انعکاس نظرها و ایده هام کنم تا از این طریق بتونم اونا رو با شما هم به اشتراک بذارم. تو این سایت قصد دارم که از فعالیت ها, سرگرمی ها, علایق و تجربه ها و خاطراتم دلنوشته هایی رو با شما به اشتراک بذارم.</p>
+              <a href="#" class="card-link btn btn-info float-left">ادامه مطلب...</a>
+            </div>
+          </div><!--card1-->
+        </div><!--col4-->
+      </div><!--row-->
+
+      <div class="footer text-center bg-dark text-light py-3 mb-1">
+        <p class="lead text-capitalize my-auto">Designed By mohammadhoseein misaghpoor</p>
+      </div><!--footer-->
     </div><!--container-->
 
   </body>
