c1abc72883a12758e40cd989302339a607388857
MohammadHossein MisaghPour <misagh1999@gmail.com>
2018-08-06 10:22:44 +0430

    Edit files name + add nabvar

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+  <head>
+    <meta charset="utf-8">
+    <title>وب سایت شخصی محمدحسین میثاق پور</title>
+    <meta name="viewport" content="width=device-width , initial-scale=1">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <script src="script/jquery.min.js"></script>
+    <script src="script/bootstrap.js"></script>
+    <link rel="stylesheet" href="font/font.css">
+  </head>
+  <body>
+<style>
+  *{
+    font-family: 'Vazir';
+  }
+</style>
+    <div class="container">
+      <div class="navbar navbar-expand">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="#" class="nav-link">صفحه اصلی</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">مقالات</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">درباره سایت</a>
+          </li>
+        </ul><!--navbar nav-->
+      </div><!--navbar-->
+    </div><!--container-->
+
+  </body>
+</html>
